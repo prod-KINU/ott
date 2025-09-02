00c16a079046a5a49baf0fe4bf1d8dd15351041e
prod-KINU <prod.kinu@gmail.com>
2025-09-02 21:44:53 +0900

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,6 +1,6 @@
 const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
-const text = 'We Love Programming!'
+const text = 'Hej! This is KINU speaking...'
 let idx = 1
 let speed = 300 / speedEl.value
 
