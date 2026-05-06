bb21d804a9b6c0a892f1dd92767124e76758a3f5
prod-KINU <prod.kinu@gmail.com>
2026-05-06 14:30:52 +0900

    site.ts を更新

--- src/data/site.ts
+++ src/data/site.ts
@@ -3,8 +3,8 @@ export type Lang = 'jp' | 'en';
 export const site = {
   name: 'KINU',
   description: {
-    jp: '映像と空間のあいだで、静かな質感を設計するクリエイティブチーム。',
-    en: 'A creative team designing quiet textures across film and spatial expression.'
+    jp: 'クリエイティブチーム : KINU',
+    en: 'Creative team : KINU'
   },
   logo: {
     dark: '/images/logo/kinu-logo.svg',
