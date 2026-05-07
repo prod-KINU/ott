dda887d22bc404dc5f74913e60387b93f028850b
prod-KINU <prod.kinu@gmail.com>
2026-05-08 01:56:44 +0900

    Update jp.ts

--- src/i18n/jp.ts
+++ src/i18n/jp.ts
@@ -13,7 +13,7 @@ export const jp = {
     homeLink: 'home'
   },
   home: {
-    eyebrow: 'creative team',
+    eyebrow: 'Yes! We Have No Bananas',
     title: 'KINU',
     intro: 'portfolio of oike, teramachi & kamo',
     cta: 'view projects',
