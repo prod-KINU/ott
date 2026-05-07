b8cbd930a51a8fcde5cffba2b28744fec9d95460
prod-KINU <prod.kinu@gmail.com>
2026-05-08 02:21:26 +0900

    profiles.ts を更新

--- src/data/profiles.ts
+++ src/data/profiles.ts
@@ -22,7 +22,7 @@ export const profiles: ProfileItem[] = [
       en: 'Profile image of oike'
     },
     bio: {
-      jp: '1993年生まれ。富山県南砺市出身。\n立命館大学卒。富山県在住。\n映像の撮影から編集・加工まで担当しています。\n映像の設計と視覚表現を中心に、対象の輪郭が静かに立ち上がるような画づくりを行います。',
+      jp: '1993年生まれ。富山県南砺市出身。\n立命館大学卒業。富山県在住。\n映像の撮影から編集・加工まで担当しています。\n映像の設計と視覚表現を中心に、対象の輪郭が静かに立ち上がるような画づくりを行います。',
       en: 'Born in 1993 in Nanto, Toyama.\nGraduated from Ritsumeikan University. Based in Toyama.\nHandles video shooting, editing, and post-production.\nWith a focus on visual planning and expression, creates images in which the contours of the subject quietly come into view.'
     },
     expertise: ['AfterEffects', 'PremierePro', 'Illustrator', 'AbletonLive']
@@ -58,7 +58,7 @@ export const profiles: ProfileItem[] = [
       en: 'Profile image of kamo'
     },
     bio: {
-      jp: '1993年生まれ。富山県砺波市出身。\n中部大学出身。富山県在住。\n楽曲制作及び3Dモデリングを担当しています。\n音の構成と展開設計を中心に、作品全体のリズムや温度感を整えるサウンドメイクを行います。',
+      jp: '1993年生まれ。富山県砺波市出身。\n中部大学卒業。富山県在住。\n楽曲制作及び3Dモデリングを担当しています。\n音の構成と展開設計を中心に、作品全体のリズムや温度感を整えるサウンドメイクを行います。',
       en: 'Born in 1993 in Tonami, Toyama.\nStudied at Chubu University. Based in Toyama.\nResponsible for music production and 3D modeling.\nThrough sound structure and development, shapes the rhythm and atmosphere of each work as a whole.'
     },
     expertise: ['Cubase', 'Blender', 'Illustrator']
